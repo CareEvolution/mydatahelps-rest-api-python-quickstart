4aad66559b639d6979729ed8e159c8ef39b88b1e
lynnfaraday <linda@careevolution.com>
2024-04-29 16:56:33 -0400

    Get specific ppt example and other clarifications.

--- quickstart.py
+++ quickstart.py
@@ -13,7 +13,7 @@ private_key = os.getenv('RKS_PRIVATE_KEY')
 service_account_name = os.getenv('RKS_SERVICE_ACCOUNT')
 project_id = os.getenv('RKS_PROJECT_ID')
 
-def get_token():
+def get_service_access_token():
     token_url = 'https://mydatahelps.org/identityserver/connect/token' 
 
     assertion = {
@@ -35,9 +35,9 @@ def get_token():
     return response.json()["access_token"]
 
 
-def get_from_api(
+# Query all participants
+def get_participants(
     access_token: str,
-    resource_url: str,
     query_params: Optional[Dict[str, str]] = None
 ) -> requests.Response:
     if query_params is None:
@@ -48,15 +48,43 @@ def get_from_api(
         "Accept": "application/json",
         "Content-Type":  "application/json; charset=utf-8"
     }
-    url = f'https://mydatahelps.org/api/v1/administration/projects/{project_id}{resource_url}'
+    url = f'https://mydatahelps.org/api/v1/administration/projects/{project_id}/participants'
 
     response = requests.get(url=url, params=query_params, headers=headers)
     response.raise_for_status()
     return response
 
-token = get_token()
-print(f'Obtained access token:\n{token}')
+# Query for a specific participant by participant identifier
+def get_participant(
+    access_token: str,
+    participant_identifier: str
+) -> requests.Response:
 
-data = get_from_api(token, '/participants')
+    headers = {
+        "Authorization": f'Bearer {access_token}',
+        "Accept": "application/json",
+        "Content-Type":  "application/json; charset=utf-8"
+    }
+    url = f'https://mydatahelps.org/api/v1/administration/projects/{project_id}/participants/{participant_identifier}'
+
+    response = requests.get(url=url, headers=headers)
+    if response.status_code == 404:
+        return None
+    response.raise_for_status()
+    return response
+    
+token = get_service_access_token()
+print(f'Obtained service access token:\n{token}')
+
+data = get_participants(token)
 participants = data.json()['totalParticipants']
 print(f'\n\nTotal participants: {participants}')
+
+participant_id = "PT-123"
+data = get_participant(token, participant_id)
+if data == None:
+  print(f'Participant {participant_id} not found.')
+else:
+  participant = data.json()
+  id = participant['id']
+  print(f'Participant {participant_id} found with MDH ID {id}')
