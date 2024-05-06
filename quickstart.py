dde83f870341995f022b9abd7503ddfbeeb84b9d
lynnfaraday <linda@careevolution.com>
2024-05-06 12:26:37 -0400

    Add a comment to clarify which ID to use in token query.

--- quickstart.py
+++ quickstart.py
@@ -106,7 +106,9 @@ if participant_identifier != "YOUR_PARTICIPANT_IDENTIFIER":
   
       # NOTE: This piece is only necessary when using MyDataHelps Embeddables in a custom app. 
       # Most API use cases do NOT require a participant token.
-      # Be sure to request the correct scope(s) for your needs.
+      # Be sure to:
+      # 1. Use the internal ID field (from participant['id'] above) and NOT participant_identifier
+      # 2. Request the correct scope(s) for your needs.
       scopes = "Participant:read SurveyAnswers:read"
       participant_access_token = get_participant_access_token(service_access_token, id, scopes)
       print(f'\nObtained participant access token for {id}: {participant_access_token}')
